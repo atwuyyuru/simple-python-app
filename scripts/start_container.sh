6dff2db9facdd4bcf260c82b0521dd438701a6ce
aditya wuyyuru <aditya6549@gmail.com>
2025-01-14 16:27:40 +0100

    add files for code deploy

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull adiwuy/simple-python-flask
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 adiwuy/simple-python-flask
