012187685f68bd4aedf41a19df3c401468772292
aditya wuyyuru <aditya6549@gmail.com>
2025-01-15 00:14:51 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -e
 
-# Pull the Docker image from Docker Hub
-docker pull adiwuy/simple-python-flask
+# Fetch Docker credentials from AWS Systems Manager Parameter Store
+DOCKER_USERNAME=$(aws ssm get-parameter --name "/pyapp/docker/username" --query "Parameter.Value" --output text)
+DOCKER_PASSWORD=$(aws ssm get-parameter --name "/pyapp/docker/password" --with-decryption --query "Parameter.Value" --output text)
 
+# Docker login using the retrieved credentials
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
-# Run the Docker image as a container
-docker run -d -p 5000:5000 adiwuy/simple-python-flask
+# Pull and start the Docker container
+docker pull adiwuy/simple-python-flask:latest
+docker run -d -p 5000:5000 adiwuy/simple-python-flask:latest
