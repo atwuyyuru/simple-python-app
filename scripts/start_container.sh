55ba895408967145ac257c79b093b0c7841cbfc3
aditya wuyyuru <aditya6549@gmail.com>
2025-01-14 16:41:25 +0100

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,5 +4,6 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull adiwuy/simple-python-flask
 
+
 # Run the Docker image as a container
 docker run -d -p 5000:5000 adiwuy/simple-python-flask
