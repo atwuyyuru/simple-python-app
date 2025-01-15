cd7061130fe63d71d9b787b9d97df174f40d3c47
aditya wuyyuru <aditya6549@gmail.com>
2025-01-15 01:20:04 +0100

    Update app.py

--- simple-python-app/app.py
+++ simple-python-app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!' 
+    return 'Hello, world!'
 
 if __name__ == '__main__':
-    app.run()
+    # Update this line to listen on all network interfaces
+    app.run(host='0.0.0.0', port=5000)
